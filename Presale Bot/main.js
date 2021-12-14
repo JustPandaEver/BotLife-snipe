e5de58d5c518b5dfca111d6aaafc5f60e3452ea3
Derek Curry <djcurr@aol.com>
2021-12-14 13:55:15 -0500

    working for pinksale

--- Presale Bot/main.js
+++ Presale Bot/main.js
@@ -3,6 +3,7 @@ const config = require('./config.json')
 const ethUtils = require('ethereumjs-util')
 
 const provider = new ethers.providers.WebSocketProvider(config["provider"]);
+const wallet = new ethers.Wallet(config["privateKey"], provider)
 
 //////////////WATCHDOG////////////////////// --> Make sure we stay connected
 const EXPECTED_PONG_BACK = 30000
@@ -15,36 +16,34 @@ async function storage(storageInt) {
 
 const wait = async () => {
   const startTime = await storage(106)
-  const minBuy = await storage(130)
-  const maxBuy = await storage(131)
-  const hardCap = await storage(133)
-  const contributedAmount = 0;
-  let nonce = await provider.getTransactionCount('0x' + ethUtils.privateToAddress(Buffer.from(config["privateKey"].trim().toLowerCase(), 'hex')).toString('hex'))
-  
-
-  while (startTime - unix() > 2) { // MAKE THIS <
+  const minBuy = (await storage(130)).toString()
+  const maxBuy = (await storage(131)).toString()
+  const hardCap = ethers.BigNumber.from((await storage(133)).toString())
+  let contributedAmount = ethers.BigNumber.from(0);
+  let nonce = (await provider.getTransactionCount('0x' + ethUtils.privateToAddress(Buffer.from(config["privateKey"].trim().toLowerCase(), 'hex')).toString('hex'))) // quite janky just make sure to use a fresh wallet
+  while (startTime - unix() > 2) { 
     process.stdout.write(startTime - unix() + " Seconds remaining\n");
     await new Promise(r => setTimeout(r, 500));
   }
   provider.on('pending', async (txHash) => { // Look through the mempool
       provider.getTransaction(txHash).then(async (tx) => { // Get the transaction from the hash
           if (tx && tx.to) {
-            if (tx.to === config["presaleAddress"] && tx[value].lte(maxBuy) && tx[value].gte(minBuy)){
-            contributedAmount += tx[value].toNumber()
-            if (contributedAmount >= hardCap.toNumber() * (config["percentFill"]/100)) {
+            if (tx.to === config["presaleAddress"] && tx.value.lte(maxBuy) && tx.value.gte(minBuy)) {
+            contributedAmount = tx.value.add(contributedAmount)
+            if (contributedAmount.gte(hardCap.mul(config["percentFill"]).div(100))) {
               let sendTx = { // Create the transaction
-                value: config["amtBuy"],
+                value: ethers.BigNumber.from(config["amtBuy"]).mul(ethers.BigNumber.from(10).pow(18)).div(1000),
                 nonce: nonce,
-                gasPrice: tx[gasPrice].minus(1),
-                gasLimit: tx.gasLimit,
+                gasPrice: (tx.gasPrice).add(1),
+                gasLimit: (tx.gasLimit),
                 chainId: parseInt(config["chainId"], 10),
                 to: config["presaleAddress"],
                 data: "0x"
               }
-              const signer = new ethers.Wallet(config["privateKey"], provider) // Create the signer from the wallet
-              let signedTx = await signer.signTransaction(sendTx) // Sign the transaction
+              let signedTx = await wallet.signTransaction(sendTx) // Sign the transaction
               let receipt = await provider.sendTransaction(signedTx) // Send the transaction to the network
               console.log("Buy receipt: " + receipt.hash + "\n");
+              process.exit(1)
             }
           }
           }
