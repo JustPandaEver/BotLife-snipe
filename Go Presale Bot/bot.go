b6cad5b35b44ae60ae9238e6a8b96ad4f5c74803
djcurr <djcurr@aol.com>
2021-12-19 16:23:10 -0500

    implement getstorage

--- Go Presale Bot/bot.go
+++ Go Presale Bot/bot.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://bsc-dataseed1.defibit.io/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	address := common.HexToAddress("0x9aeCccc1e5E4172c849895CA6351e5DD359e1C72")
+	num := []byte("131")
+	key := common.BytesToHash(num)
+	val, err := client.StorageAt(context.Background(), address, key, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(val)
 }
